de5a57a61b789cdc1da69ac18eb810c46f05fd21
AnweshaMajumder <anwesha.majumdar@gmail.com>
2023-10-23 15:24:59 +0530

    first commit

---
+++ firstSpringBootJava/src/main/java/com/vanillatechie/firstSpringBootJava/FirstSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package com.vanillatechie.firstSpringBootJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootJavaApplication.class, args);
+	}
+
+}
---
+++ firstSpringBootJava/src/test/java/com/vanillatechie/firstSpringBootJava/FirstSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanillatechie.firstSpringBootJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
